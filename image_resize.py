4a7b8a3b341953973a74c0ee9ffd11bb6ff6c0f9
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-07-06 02:33:07 +0300

    introduced main() code complexity decreased from 8 to 7

--- image_resize.py
+++ image_resize.py
@@ -9,13 +9,13 @@ def resize_image(old_image_path: "str", new_size: "tuple", new_dir: "str") -> "s
         new_image = image.resize(new_size, Image.ANTIALIAS)
         new_file_ext = image.format.lower()
     _, file_name = os.path.split(old_image_path)
-    new_file_name = "{}_{}x{}.{}".format(file_name.split('.')[0], new_width, new_height, new_file_ext)
+    new_file_name = "{}_{}x{}.{}".format(file_name.split('.')[0], new_size[0], new_size[1], new_file_ext)
     new_file_path = os.path.join(new_dir, new_file_name)
     new_image.save(new_file_path)
     return new_file_path
 
 
-def check_wxh_format(wxh: "str") -> "tuple of ints":
+def check_wxh_format(wxh: "str") -> "tuple":
     pars = wxh.lower().split('x')
     try:
         width = int(pars[0])
@@ -37,43 +37,47 @@ def check_image_file(image_file: "str") -> "str":
         return image_file
 
 
-if __name__ == '__main__':
-    ap = argparse.ArgumentParser(description='resize .jpg and .png images')
-    ap.add_argument("image_file", type=check_image_file, help="path to .jpg or .png image file")
-    resize = ap.add_mutually_exclusive_group(required=True)
-    resize.add_argument("--width", dest="width", action="store", type=int, help="width of resized picture")
-    resize.add_argument("--height", dest="height", action="store", type=int, help="height of resized picture")
-    resize.add_argument("--wxh", dest="wxh", action="store", type=check_wxh_format, help="new aspect, format: 200x300")
-    resize.add_argument("--scale", dest="scale", action="store", type=float, help="scale of picture resizing")
-    ap.add_argument("--output", dest="out_path", action="store", help="folder where to put resized picture")
-    args = ap.parse_args(sys.argv[1:])
-
-    with Image.open(args.image_file) as old_image:
+def main(file, out_path, width, height, wxh, scale):
+    with Image.open(file) as old_image:
         old_width, old_height = old_image.size
-        old_format = old_image.format
 
     aspect_ratio = round(old_width / old_height, 2)
     # set output directory path
-    new_path, _ = os.path.split(args.image_file)
+    new_path, _ = os.path.split(file)
     if not new_path:
         new_path = os.getcwd()
-    if args.out_path:
-        if not os.path.isdir(args.out_path):
-            print("{} is not a valid output path".format(args.out_path))
+    if out_path:
+        if not os.path.isdir(out_path):
+            print("{} is not a valid output path".format(out_path))
             exit()
         else:
-            new_path = args.out_path
+            new_path = out_path
     # define new image size
-    if args.scale:
-        new_width = int(old_width * args.scale)
-        new_height = int(old_height * args.scale)
-    elif args.width:
-        new_width = args.width
+    if scale:
+        new_width = int(old_width * scale)
+        new_height = int(old_height * scale)
+    elif width:
+        new_width = width
         new_height = int(new_width / aspect_ratio)
-    elif args.height:
-        new_height = args.height
+    elif height:
+        new_height = height
         new_width = int(new_height * aspect_ratio)
     else:  # new ratio wxh
-        new_width, new_height = args.wxh
+        new_width, new_height = wxh
 
-    print("re-sized imaged saved to {}".format(resize_image(args.image_file, (new_width, new_height), new_path)))
+    print("re-sized imaged saved to {}".format(resize_image(file, (new_width, new_height), new_path)))
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser(description='resize .jpg and .png images')
+    ap.add_argument("image_file", type=check_image_file, help="path to .jpg or .png image file")
+    mgroup = ap.add_mutually_exclusive_group(required=True)
+    mgroup.add_argument("--width", dest="width", action="store", type=int, help="width of resized picture")
+    mgroup.add_argument("--height", dest="height", action="store", type=int, help="height of resized picture")
+    mgroup.add_argument("--wxh", dest="wxh", action="store", type=check_wxh_format, help="new aspect, format: 200x300")
+    mgroup.add_argument("--scale", dest="scale", action="store", type=float, help="scale of picture resizing")
+    ap.add_argument("--output", dest="out_path", action="store", help="folder where to put resized picture")
+    args = ap.parse_args(sys.argv[1:])
+
+    main(args.image_file, args.out_path, args.width, args.height, args.wxh, args.scale)
+
