0ebf2b9eb32e6c19d17533427d6accc26475e2b5
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-07-06 17:09:22 +0300

    renaming changed resize_image()

--- image_resize.py
+++ image_resize.py
@@ -28,7 +28,7 @@ def check_image_file(image_file: "str") -> "str":
         return image_file
 
 
-def resize_image(old_image: "class 'PIL.JpegImagePlugin.JpegImageFile'", width: "int", height: "int", wxh: "int",
+def resize_image(old_image: "class 'PIL.JpegImagePlugin.JpegImageFile'", width: "int", height: "int", wxh: "tuple",
                  scale: "float") -> "tuple":
     old_width, old_height = old_image.size
     aspect_ratio = round(old_width / old_height, 2)
@@ -47,7 +47,7 @@ def resize_image(old_image: "class 'PIL.JpegImagePlugin.JpegImageFile'", width: 
     return old_image.resize(new_size, Image.ANTIALIAS), new_size
 
 
-def main(image_file_name: "str", new_path: "str", width: "int", height: "int", wxh: "int", scale: "float"):
+def main(image_file_name: "str", new_path: "str", width: "int", height: "int", wxh: "tuple", scale: "float"):
     with Image.open(image_file_name) as old_image:
         new_image, new_size = resize_image(old_image, width, height, wxh, scale)
         new_file_ext = old_image.format.lower()
@@ -65,12 +65,12 @@ def main(image_file_name: "str", new_path: "str", width: "int", height: "int", w
 if __name__ == '__main__':
     ap = argparse.ArgumentParser(description='resize .jpg and .png images')
     ap.add_argument("image_file", type=check_image_file, help="path to .jpg or .png image file")
-    mgroup = ap.add_mutually_exclusive_group(required=True)
-    mgroup.add_argument("--width", dest="width", action="store", type=int, help="width of resized picture")
-    mgroup.add_argument("--height", dest="height", action="store", type=int, help="height of resized picture")
-    mgroup.add_argument("--wxh", dest="wxh", action="store", type=check_wxh_format, help="new aspect, format: 200x300")
-    mgroup.add_argument("--scale", dest="scale", action="store", type=float, help="scale of picture resizing")
-    ap.add_argument("--output", dest="out_path", action="store", help="folder where to put resized picture")
+    meg = ap.add_mutually_exclusive_group(required=True)
+    meg.add_argument("--width", dest="width", action="store", type=int, help="width of re-sized picture")
+    meg.add_argument("--height", dest="height", action="store", type=int, help="height of re-sized picture")
+    meg.add_argument("--wxh", dest="wxh", action="store", type=check_wxh_format, help="new aspect, format: 200x300")
+    meg.add_argument("--scale", dest="scale", action="store", type=float, help="scale of picture resizing")
+    ap.add_argument("--output", dest="out_path", action="store", help="folder where to put re-sized picture")
     args = ap.parse_args(sys.argv[1:])
     if args.out_path and not os.path.isdir(args.out_path):
         print("{} is not a valid output path".format(args.out_path))
